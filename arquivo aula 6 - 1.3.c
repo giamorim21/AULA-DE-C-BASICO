c1455fadbcb2da0a9bff0ba4b24b0fc0be24ed96
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 18:19:49 -0300

    Update arquivo aula 6 - 1.3.c

--- arquivo aula 6 - 1.3.c
+++ arquivo aula 6 - 1.3.c
@@ -4,4 +4,22 @@ e verifica quantos estão acima da média(dois casa decimal).
 
 
 void main(){
+    float vetor[MAX];
+    int contador = 0;
+    int c;
+    float m;
+    for (c = 0;c < MAX;c++) {
+       printf("Digite um valor:\n");
+       scanf("%f",&vetor[c]);
+       m += vetor[c];
+       }
+
+       m/=MAX;
+       for(c = 0;c < MAX;c++){
+          if(vetor[c]>m){
+            contador+=1;
+          }
+        }
+       printf("O valor da media eh %.2f\n",m);
+       printf("A quantidade de valores maiores que a media eh:%d",contador);
     }
