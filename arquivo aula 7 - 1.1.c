a060871d5891a8ec69032ca72a51b97077859774
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 18:58:19 -0300

    Add files via upload

---
+++ arquivo aula 7 - 1.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*faça um código que preencha uma tabela de tamanho 3 por 4
+onde cada casas sera prenchida da seguinte forma [L]*4 + [C]
+*/
+
+void main(){
+    int l,c,M[3][4], soma=0;
+    int cont;
+
+    for(l=0; l<3; ++l){
+        for(c=0; c<4; ++c){
+            cont = l*c;
+            M[3][4] = cont;
+        }}
+     for(l=0; l<3; ++l){
+        for(c=0; c<4; ++c){
+            printf("&d",M[l][c]);
+    }
+     }
+}
